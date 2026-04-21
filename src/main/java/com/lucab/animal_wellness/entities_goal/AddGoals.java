921db5351cdedc5f088d5887331cb3c1ef878169
Luca Bertaggia <luca.bertaggia82@gmail.com>
2026-04-21 11:52:52 +0200

    Removed tempt goal from animals

--- src/main/java/com/lucab/animal_wellness/entities_goal/AddGoals.java
+++ src/main/java/com/lucab/animal_wellness/entities_goal/AddGoals.java
@@ -2,6 +2,7 @@ package com.lucab.animal_wellness.entities_goal;
 
 import com.lucab.animal_wellness.AnimalWellness;
 import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.goal.TemptGoal;
 import net.minecraft.world.entity.animal.Animal;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -14,6 +15,10 @@ public class AddGoals {
         if (event.getEntity() instanceof PathfinderMob mob && event.getEntity() instanceof Animal && !event.getLevel().isClientSide()) {
             mob.goalSelector.addGoal(1, new EscapePlayerGoal(mob));
             mob.goalSelector.addGoal(2, new FeedGoal(mob));
+
+            mob.goalSelector.getAvailableGoals().removeIf(goal ->
+                    goal.getGoal() instanceof TemptGoal
+            );
         }
     }
 }
