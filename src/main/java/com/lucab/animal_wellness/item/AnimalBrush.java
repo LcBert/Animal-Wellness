7acee3f767d86f0ec27b5a7876a68e89471a4b2d
Luca Bertaggia <luca.bertaggia82@gmail.com>
2026-04-25 03:49:37 +0200

    Added particle effects to animal that need brushing if player has brush in hand

--- src/main/java/com/lucab/animal_wellness/item/AnimalBrush.java
+++ src/main/java/com/lucab/animal_wellness/item/AnimalBrush.java
@@ -1,15 +1,23 @@
 package com.lucab.animal_wellness.item;
 
 
+import com.lucab.animal_wellness.AnimalWellness;
 import com.lucab.animal_wellness.attachments.WellnessHelper;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+
+import java.util.List;
 
 public class AnimalBrush extends Item {
     public AnimalBrush() {
@@ -24,6 +32,25 @@ public class AnimalBrush extends Item {
     @Override
     public int getEnchantmentValue(ItemStack stack) {
         return 15;
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, Level level, Entity entity, int slot, boolean isSelected) {
+        if (!level.isClientSide && entity.tickCount % 20 == 0 && isSelected && entity instanceof Player player) {
+            ServerLevel serverLevel = (ServerLevel) level;
+            AABB box = entity.getBoundingBox().inflate(10);
+
+            List<Entity> entities = level.getEntities(player, box, checkEntity -> {
+                WellnessHelper helper = WellnessHelper.getInstance(checkEntity);
+                return helper.isConsideredAnimal() && !helper.isBrushed();
+            });
+
+            entities.forEach(checkEntity -> {
+                serverLevel.sendParticles(ParticleTypes.HAPPY_VILLAGER,
+                        checkEntity.getX(), checkEntity.getY() + 0.5, checkEntity.getZ(),
+                        5, 0.2, 0.2, 0.2, 0.1);
+            });
+        }
     }
 
     @Override
